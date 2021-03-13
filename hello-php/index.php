3066982578c581e5f305bc595502e1a1d75cc0c1
christian candelaria <christiancandelaria@christians-MacBook-Air.local>
2021-03-13 14:13:53 +0100

    Added database index page for php

--- hello-php/index.php
+++ hello-php/index.php
@@ -1,8 +1,14 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World new upodate repo</p>'; ?> 
- </body>
-</html>
+<?php
+$SERVERNAME = "localhost";
+$USERNAME = "username";
+$PASSWORD = "password";
+
+// Create connection
+$conn = new mysqli($SERVERNAME, $USERNAME, $PASSWORD);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
